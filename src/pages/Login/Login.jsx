5550fe19370f86b63b943343536874026e6de22a
Nguyenthi-8796 <ndthi8796@gmail.com>
2025-12-13 14:11:37 +0700

    login, signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,60 +2,67 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle, faApple, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import styles from './Login.module.scss';
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import styles from './Login.module.scss';
 
 const Login = () => {
     return (
         <div className={styles.loginContainer}>
-            <div className={styles.brandLogo}>augustine</div>
+            <div className={clsx(styles.brandLogo, styles.animateEnter)} style={{ animationDelay: '0.1s' }}>
+                augustine
+            </div>
 
             <div className={styles.loginCard}>
-                <Link to ="/" className={styles.btnBack}>
-                    <FontAwesomeIcon icon={faChevronLeft} /> Back
+                <Link to="/" className={clsx(styles.btnBack, styles.animateEnter)} style={{ animationDelay: '0.2s' }}>
+                    <FontAwesomeIcon icon={faChevronLeft}/> Back
                 </Link>
 
-                <div className={styles.headerText}>
+                <div className={clsx(styles.headerText, styles.animateEnter)} style={{ animationDelay: '0.3s' }}>
                     <h1>Welcome back!</h1>
                     <p>We're glad to see you again</p>
                 </div>
                 
                 <form className={styles.loginForm}>
-                    <div className={styles.inputGroup}>
+                    <div className={clsx(styles.inputGroup, styles.animateEnter)} style={{ animationDelay: '0.4s' }}>
                         <input type="text" placeholder="Email/Phone Number" required />
                     </div>
-                    
-                    <div className={styles.inputGroup}>
+                    <div className={clsx(styles.inputGroup, styles.animateEnter)} style={{ animationDelay: '0.5s' }}>
                         <input type="password" placeholder="Password" required />
                     </div>
 
-                    <button type="submit" className={styles.submitBtn}>
+                    <button type="submit" className={clsx(styles.submitBtn, styles.animateEnter)} style={{ animationDelay: '0.6s' }}>
                         Sign In
                     </button>
                 </form>
 
-                <div className={styles.socialLogin}>
+                <div className={clsx(styles.socialLogin, styles.animateEnter)} style={{ animationDelay: '0.7s' }}>
                     <p className={styles.dividerText}>or sign in with</p>
+
                     <div className={styles.socialIcons}>
-                        <button className={`${styles.socialBtn} ${styles.google}`}>
+                        <button className={clsx(styles.socialBtn, styles.google)}>
                             <FontAwesomeIcon icon={faGoogle} />
                         </button>
                         
-                        <button className={`${styles.socialBtn} ${styles.apple}`}>
+                        <button className={clsx(styles.socialBtn, styles.apple)}>
                             <FontAwesomeIcon icon={faApple} />
                         </button>
                         
-                        <button className={`${styles.socialBtn} ${styles.facebook}`}>
+                        <button className={clsx(styles.socialBtn, styles.facebook)}>
                             <FontAwesomeIcon icon={faFacebookF} />
                         </button>
                     </div>
                 </div>
-                <div className={styles.footerLink}>
-                    <p>Don't have an account? <Link to ="/signup">Sign up</Link></p>
+
+                <div 
+                    className={clsx(styles.footerLink, styles.animateEnter)}
+                    style={{ animationDelay: '0.8s' }}
+                >
+                    <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,44 +1,48 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle, faApple, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import styles from './SignUp.module.scss';
 import { Link } from 'react-router-dom';
+import clsx from 'clsx';
 
 const SignUp = () => {
     return (
         <div className={styles.signUpContainer}>
-            <div className={styles.brandLogo}>augustine </div>
+            <div className={clsx(styles.brandLogo, styles.animateEnter)} style={{ animationDelay: '0.1s' }}>
+                augustine 
+            </div>
 
             <div className={styles.signUpCard}>
-                <Link to="/login" className={styles.btnBack}>
+                <Link to="/login" className={clsx(styles.btnBack, styles.animateEnter)} style={{ animationDelay: '0.2s' }}>
                     <FontAwesomeIcon icon={faChevronLeft} /> Back
                 </Link>
-                <div className={styles.headerText}>
+
+                <div className={clsx(styles.headerText, styles.animateEnter)} style={{ animationDelay: '0.3s' }}>
                     <h1>Welcome to <span className={styles.italicText}> augustine </span>!</h1>
                     <p>Start by creating an account</p>
                 </div>
 
                 <form className={styles.signUpForm}>
-                    <div className={styles.inputGroup}>
+                    <div className={clsx(styles.inputGroup, styles.animateEnter)} style={{ animationDelay: '0.4s' }}>
                         <input type="text" placeholder="Email/Phone Number" required />
                     </div>
 
-                    <div className={styles.inputGroup}>
+                    <div className={clsx(styles.inputGroup, styles.animateEnter)} style={{ animationDelay: '0.5s' }}>
                         <input type="password" placeholder="Password" required />
                     </div>
-                    <button type="submit" className={styles.submitBtn}>
+
+                    <button type="submit" className={clsx(styles.submitBtn, styles.animateEnter)} style={{ animationDelay: '0.6s' }}>
                         Creating Account
                     </button>
                 </form>
-                <div className={styles.acceptTerms}>
+
+                <div className={clsx(styles.acceptTerms, styles.animateEnter)} style={{ animationDelay: '0.7s' }}>
                     <p>By clicking on "Create Account", you agree to our Terms and Privacy Policy</p>
                 </div>
 
-                <div className={styles.toLogin}>
+                <div className={clsx(styles.toLogin, styles.animateEnter)} style={{ animationDelay: '0.8s' }}>
                     <p>Have an account already? <Link to ="/login">Log in</Link></p>
                 </div>
-
             </div>
         </div>
     )
