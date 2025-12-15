b6afb1396f990bf74342aa8a4b54c9417947763e
Manh2005 <tranduymanhtd2@gmail.com>
2025-12-15 15:51:29 +0700

    cart

--- src/Layouts/components/Header/Header.jsx
+++ src/Layouts/components/Header/Header.jsx
@@ -17,10 +17,10 @@ const Header = () => {
   // kết quả tìm kieemms khi call api:
   //const [searchResult, setSearchResult] = useState([1]);
 
-  const [openCenter, setOpenCenter] = useState(false)
+  const [openCenter, setOpenCenter] = useState(false);
 
-  function handleCloseChat(){
-    setOpenCenter(false)
+  function handleCloseChat() {
+    setOpenCenter(false);
   }
   return (
     <>
@@ -30,33 +30,43 @@ const Header = () => {
         </Modal>
       )}
       <header className="wrapper_header">
-      <div className={clsx(styles.header_wrap, "grid", "wide")}>
-        <div className={clsx(styles.header_left_wrap)}>
-          <div className={clsx(styles.logo)}>
-            <Link to="/" className={styles.name_web}>augustine</Link>
+        <div className={clsx(styles.header_wrap, "grid", "wide")}>
+          <div className={clsx(styles.header_left_wrap)}>
+            <div className={clsx(styles.logo)}>
+              <Link to="/" className={styles.name_web}>
+                augustine
+              </Link>
+            </div>
+
+            <Menu />
+            <Collections />
           </div>
 
-          <Menu />
-          <Collections />
+          <SearchForm />
+
+          <div className={clsx(styles.header_actions)}>
+            <CartHeader />
+            <Button
+              className={styles.contact}
+              primary
+              size="medium"
+              onClick={() => setOpenCenter(true)}
+            >
+              Contact Us
+            </Button>
+
+            <div className={styles.favourite}>
+              <FontAwesomeIcon icon={faHeart} className={styles.fav_icon} />
+            </div>
+            <div className={styles.ava}>
+              <img
+                src="\assets\531d8d6e6b86f200b46cf8cf07cbcbf7.jpg"
+                alt="ava"
+              />
+            </div>
+          </div>
         </div>
-
-        <SearchForm />
-
-        <div className={clsx(styles.header_actions)}>
-          <CartHeader />
-          <Button className={styles.contact} primary size="medium" onClick={() => setOpenCenter(true)}>
-            Contact Us
-          </Button>
-          
-          <div className={styles.favourite}>
-            <FontAwesomeIcon icon={faHeart} className={styles.fav_icon}/>
-          </div>
-          <div className={styles.ava}>
-            <img src="https://scontent.fhan2-5.fna.fbcdn.net/v/t39.30808-1/503186511_2260630544391853_5985719783730471123_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=106&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeHlpNM4djcVLI-k89gHWoDnuOM1rkHCe_a44zWuQcJ79lqd2eOIY1pZNgw53uXPWZerZrRB7Lfl_SBzf60DFtZ_&_nc_ohc=VeSUMVPZqKgQ7kNvwFmqdwj&_nc_oc=AdkHmSgsLaxrYsNDxKSxuG6-uD8mSWD5Kfa-bl-0ZrZypxyHcqjDiK1dtsEoJq3FHXo&_nc_zt=24&_nc_ht=scontent.fhan2-5.fna&_nc_gid=aGy5NdPy1IKbAzzVt0x0Xg&oh=00_AfmgSMP-e5lBivWraIXX6P49FjiJv5CFlfuSrDXfT4bJFg&oe=693D8C0F" alt="ava" />
-          </div>
-        </div>
-      </div>
-    </header>
+      </header>
     </>
   );
 };
