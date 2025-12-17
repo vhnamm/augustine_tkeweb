540f59186e0698b9366f14086c9ecec271a3071f
Manh2005 <tranduymanhtd2@gmail.com>
2025-12-18 01:06:01 +0700

    1gio05 12/18/2025

--- src/components/routes/routes.jsx
+++ src/components/routes/routes.jsx
@@ -24,7 +24,9 @@ import Step1_Account from "~/pages/SignUp/SignUp_Step1";
 import Step2_Name from "~/pages/SignUp/SignUp_Step2";
 import Step3_Categories from "~/pages/SignUp/SignUp_Step3";
 import Step4_Completed from "~/pages/SignUp/SignUp_Step4";
-import OrderPlaced from "../../pages/OrderPlaced/OrderPlaced";        
+import OrderPlaced from "../../pages/OrderPlaced/OrderPlaced";   
+import PayMent from "~/pages/PayMent/PayMent";     
+import QrPayment from "~/pages/QrPayment/QrPayment";
 export const routes = (
   <>
     <Route path="/" element={<MainLayout />}>
@@ -50,6 +52,8 @@ export const routes = (
       <Route path="cart" element={<Cart />} />
       <Route path="checkout" element={<Checkout />} />
       <Route path="order-placed" element={<OrderPlaced />} />
+      <Route path="payment" element={<PayMent />} />
+      <Route path="qr-payment" element={<QrPayment />} />
     </Route>
 
     <Route path="/login" element={<Login />} />
