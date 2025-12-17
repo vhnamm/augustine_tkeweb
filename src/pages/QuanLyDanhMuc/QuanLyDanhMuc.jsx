4c15691b75952d4e40b402bddc972e8daf84af87
vhnamm <nammango123@gmail.com>
2025-12-18 01:41:36 +0700

    finish admin overview

---
+++ src/pages/QuanLyDanhMuc/QuanLyDanhMuc.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './QuanLyDanhMuc.module.scss'
+
+const QuanLyDanhMuc = () => {
+  return (
+    <div>
+      Quan ly danh muc sp
+    </div>
+  );
+};
+
+export default QuanLyDanhMuc;
